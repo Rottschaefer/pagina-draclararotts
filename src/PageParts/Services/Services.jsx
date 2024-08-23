4ef5c52bf0f3601742e170adbd0f12ad2591d590
Rottschaefer <rottschaefer54@gmail.com>
2024-08-23 18:16:13 -0300

    Fixing services images paths

--- src/PageParts/Services/Services.jsx
+++ src/PageParts/Services/Services.jsx
@@ -1,14 +1,20 @@
 import { StyledServices } from "./StyledServices";
 import { Carousel } from "../../Components/Carousel/Carousel";
+import img1 from "../../assets/1.jpg";
+import img2 from "../../assets/2.jpg";
+import img3 from "../../assets/3.jpg";
+import img4 from "../../assets/4.jpg";
+import img5 from "../../assets/5.jpg";
+import img6 from "../../assets/6.jpg";
 
 export const ServicesPage = () => {
   const imgs = [
-    ["src/assets/1.jpg", "dor aguda/crônica"],
-    ["src/assets/2.jpg", "hipertensão"],
-    ["src/assets/3.jpg", "colesterol alto"],
-    ["src/assets/4.jpg", "sobrepeso/obesidade"],
-    ["src/assets/5.jpg", "atendimento domiciliar"],
-    ["src/assets/6.jpg", "exames de rotina"],
+    [img1, "dor aguda/crônica"],
+    [img2, "hipertensão"],
+    [img3, "colesterol alto"],
+    [img4, "sobrepeso/obesidade"],
+    [img5, "atendimento domiciliar"],
+    [img6, "exames de rotina"],
   ];
   return (
     <StyledServices>
