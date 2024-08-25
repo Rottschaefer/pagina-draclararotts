d282334251f360f197040058b985bb3c5ad285d0
Rottschaefer <rottschaefer54@gmail.com>
2024-08-25 14:29:51 -0300

    Changed the begging page especially the image itself to a linear gradient background with a no back profile picture

--- src/PageParts/Beggining/Beggining.jsx
+++ src/PageParts/Beggining/Beggining.jsx
@@ -1,13 +1,18 @@
 import {
+  StyledBackImg,
   StyledBeggining,
   StyledButton,
   StyledTitle,
   StyledTitleDiv,
 } from "./StyledBeggining";
+import background from "../../assets/perfil_no_back.png";
 
 export const Beggining = () => {
   return (
     <StyledBeggining>
+      {/* <StyledBackImgDiv> */}
+      <StyledBackImg src={background} />
+      {/* </StyledBackImgDiv> */}
       <StyledTitleDiv>
         <StyledTitle>{"O caminho para o seu".toLocaleUpperCase()}</StyledTitle>
         <StyledTitle>{"bem-estar começa aqui".toLocaleUpperCase()}</StyledTitle>
